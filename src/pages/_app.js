ccb5b17e6eee561013726a5f618f61a7e1099c47
Dolores Polito <dolores.polito@gmail.com>
2023-08-15 18:43:56 -0300

    web description

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -16,7 +16,7 @@ const MyApp = ({ Component, pageProps }) => {
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
-        <meta name="description" content="Edición y Escritura" />
+        <meta name="description" content="TURISMO & FOTOGRAFÍA" />
 
 
       </Head>
