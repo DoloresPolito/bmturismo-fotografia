bd7cbaa7b1e8c24a91f86100fa7004b836c0b554
Dolores Polito <dolores.polito@gmail.com>
2023-08-24 10:59:25 -0300

    ring carousel

--- src/components/CarouselProfessional.js
+++ src/components/CarouselProfessional.js
@@ -14,7 +14,6 @@ import professional4big from "../../public/assets/pictures/sectionscarousel/comp
 import professional5big from "../../public/assets/pictures/sectionscarousel/compress/retrato profesional 10x6/5.jpg";
 import professional6big from "../../public/assets/pictures/sectionscarousel/compress/retrato profesional 10x6/6.jpg";
 
-
 import professional0small from "../../public/assets/pictures/sectionscarousel/compress/retrato profesional 10x11/0.jpg";
 import professional1small from "../../public/assets/pictures/sectionscarousel/compress/retrato profesional 10x11/1.jpg";
 import professional2small from "../../public/assets/pictures/sectionscarousel/compress/retrato profesional 10x11/2.jpg";
@@ -23,8 +22,9 @@ import professional4small from "../../public/assets/pictures/sectionscarousel/co
 import professional5small from "../../public/assets/pictures/sectionscarousel/compress/retrato profesional 10x11/5.jpg";
 import professional6small from "../../public/assets/pictures/sectionscarousel/compress/retrato profesional 10x11/6.jpg";
 
+import { RotatingLines } from "react-loader-spinner";
 
-const CarouselProfessional= () => {
+const CarouselProfessional = () => {
   const [selectedImageIndex, setSelectedImageIndex] = useState(0); // Initialize with the default index
 
   const handleThumbnailClick = (index) => {
@@ -32,6 +32,16 @@ const CarouselProfessional= () => {
   };
 
   const [width, setWidth] = useState(null);
+
+  const [mostrarPrimerContenido, setMostrarPrimerContenido] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setMostrarPrimerContenido(false);
+    }, 1200);
+
+    return () => clearTimeout(timeout);
+  }, []);
 
   useEffect(() => {
     const handleResize = () => {
@@ -47,109 +57,142 @@ const CarouselProfessional= () => {
   }, []);
 
   return (
-    <CarouselSection>
-      <Carousel
-        selectedItem={selectedImageIndex}
-        onChange={(index) => setSelectedImageIndex(index)}
-        showThumbs={false}
-        showStatus={false}
-        infiniteLoop={true}
-        slideNumber={true}
-        autoPlay={true}
-        renderArrowPrev={
-          (onClickHandler, hasPrev, label) => (
-            // hasPrev && (
-            <button
-              type="button"
-              onClick={onClickHandler}
-              title={label}
-              style={{
-                position: "absolute",
-                zIndex: 2,
-                left: 15,
-                top: "60%",
-                transform: "translateY(-50%)",
-                background: "transparent",
-                border: "none",
-              }}
+    <>
+      <CarouselSection>
+        {mostrarPrimerContenido ? (
+       <RotatingLines
+       strokeColor="#C29A00"
+       strokeWidth="3"
+       animationDuration="0.75"
+       width="70"
+       visible={true}
+     />
+        ) : (
+          <>
+            <Carousel
+              selectedItem={selectedImageIndex}
+              onChange={(index) => setSelectedImageIndex(index)}
+              showThumbs={false}
+              showStatus={false}
+              infiniteLoop={true}
+              slideNumber={true}
+              autoPlay={true}
+              renderArrowPrev={
+                (onClickHandler, hasPrev, label) => (
+                  // hasPrev && (
+                  <button
+                    type="button"
+                    onClick={onClickHandler}
+                    title={label}
+                    style={{
+                      position: "absolute",
+                      zIndex: 2,
+                      left: 15,
+                      top: "60%",
+                      transform: "translateY(-50%)",
+                      background: "transparent",
+                      border: "none",
+                    }}
+                  >
+                    <Image src={leftArrowImage} alt="Flecha Izquierda" />
+                  </button>
+                )
+                // )
+              }
+              renderArrowNext={
+                (onClickHandler, hasNext, label) => (
+                  // hasNext && (
+                  <button
+                    type="button"
+                    onClick={onClickHandler}
+                    title={label}
+                    style={{
+                      position: "absolute",
+                      zIndex: 2,
+                      right: 15,
+                      top: "60%",
+                      transform: "translateY(-50%)",
+                      background: "transparent",
+                      border: "none",
+                    }}
+                  >
+                    <Image src={rightArrowImage} alt="Flecha Derecha" />
+                  </button>
+                )
+                // )
+              }
             >
-              <Image src={leftArrowImage} alt="Flecha Izquierda" />
-            </button>
-          )
-          // )
-        }
-        renderArrowNext={
-          (onClickHandler, hasNext, label) => (
-            // hasNext && (
-            <button
-              type="button"
-              onClick={onClickHandler}
-              title={label}
-              style={{
-                position: "absolute",
-                zIndex: 2,
-                right: 15,
-                top: "60%",
-                transform: "translateY(-50%)",
-                background: "transparent",
-                border: "none",
-              }}
-            >
-              <Image src={rightArrowImage} alt="Flecha Derecha" />
-            </button>
-          )
-          // )
-        }
-      >
-        <Div>
-          <Image src={width > 1000 ? professional0big : professional0small} alt="1" />
-        </Div>
-        <Div>
-          <Image src={width > 1000 ? professional1big : professional1small} alt="1" />
-        </Div>
-        <Div>
-          <Image src={width > 1000 ? professional2big : professional2small} alt="1" />
-        </Div>
-        <Div>
-          <Image src={width > 1000 ? professional3big : professional3small} alt="1" />
-        </Div>
-        <Div>
-          <Image src={width > 1000 ? professional4big : professional4small} alt="1" />
-        </Div>
-        <Div>
-          <Image src={width > 1000 ? professional5big : professional5small} alt="1" />
-        </Div>
-        <Div>
-          <Image src={width > 1000 ? professional6big : professional6small} alt="1" />
-        </Div>
-       
-      </Carousel>
+              <Div>
+                <Image
+                  src={width > 1000 ? professional0big : professional0small}
+                  alt="1"
+                />
+              </Div>
+              <Div>
+                <Image
+                  src={width > 1000 ? professional1big : professional1small}
+                  alt="1"
+                />
+              </Div>
+              <Div>
+                <Image
+                  src={width > 1000 ? professional2big : professional2small}
+                  alt="1"
+                />
+              </Div>
+              <Div>
+                <Image
+                  src={width > 1000 ? professional3big : professional3small}
+                  alt="1"
+                />
+              </Div>
+              <Div>
+                <Image
+                  src={width > 1000 ? professional4big : professional4small}
+                  alt="1"
+                />
+              </Div>
+              <Div>
+                <Image
+                  src={width > 1000 ? professional5big : professional5small}
+                  alt="1"
+                />
+              </Div>
+              <Div>
+                <Image
+                  src={width > 1000 ? professional6big : professional6small}
+                  alt="1"
+                />
+              </Div>
+            </Carousel>
 
-      <SmallImagesContainer>
-        <div onClick={() => handleThumbnailClick(0)}>
-          <Image src={professional0big} alt="Miniatura 1" />
-        </div>
-        <div onClick={() => handleThumbnailClick(1)}>
-          <Image src={professional1big} alt="Miniatura 2" />
-        </div>
-        <div onClick={() => handleThumbnailClick(2)}>
-          <Image src={professional2big} alt="Miniatura 3" />
-        </div>
-        <div onClick={() => handleThumbnailClick(3)}>
-          <Image src={professional3big} alt="Miniatura 4" />
-        </div>
-        <div onClick={() => handleThumbnailClick(4)}>
-          <Image src={professional4big} alt="Miniatura 5" />
-        </div>
-        <div onClick={() => handleThumbnailClick(5)}>
-          <Image src={professional5big} alt="Miniatura 5" />
-        </div>
-        <div onClick={() => handleThumbnailClick(6)}>
-          <Image src={professional6big} alt="Miniatura 5" />
-        </div>
-       
-      </SmallImagesContainer>
-    </CarouselSection>
+            <SmallImagesContainer>
+              <div onClick={() => handleThumbnailClick(0)}>
+                <Image src={professional0big} alt="Miniatura 1" />
+              </div>
+              <div onClick={() => handleThumbnailClick(1)}>
+                <Image src={professional1big} alt="Miniatura 2" />
+              </div>
+              <div onClick={() => handleThumbnailClick(2)}>
+                <Image src={professional2big} alt="Miniatura 3" />
+              </div>
+              <div onClick={() => handleThumbnailClick(3)}>
+                <Image src={professional3big} alt="Miniatura 4" />
+              </div>
+              <div onClick={() => handleThumbnailClick(4)}>
+                <Image src={professional4big} alt="Miniatura 5" />
+              </div>
+              <div onClick={() => handleThumbnailClick(5)}>
+                <Image src={professional5big} alt="Miniatura 5" />
+              </div>
+              <div onClick={() => handleThumbnailClick(6)}>
+                <Image src={professional6big} alt="Miniatura 5" />
+              </div>
+            </SmallImagesContainer>
+          </>
+        )}
+      </CarouselSection>
+    </>
   );
 };
 
@@ -158,7 +201,7 @@ const CarouselSection = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-top: 30px;
+  height: 130vh;
 
   @media screen and (max-width: 600px) {
     button {
